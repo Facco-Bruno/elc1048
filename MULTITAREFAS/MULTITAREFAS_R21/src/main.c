d2822a2638838bd95ff076fbb12414917e1017d1
Bruno <bruno.facco@acad.ufsm.br>
2021-07-21 12:44:05 -0300

    Update main.c
    Criação da Tarefa 9

--- MULTITAREFAS/MULTITAREFAS_R21/src/main.c
+++ MULTITAREFAS/MULTITAREFAS_R21/src/main.c
@@ -40,6 +40,7 @@ void tarefa_5(void);
 void tarefa_6(void);
 void tarefa_7(void);
 void tarefa_8(void);
+void tarefa_9(void);
 
 /*
  * Configuracao dos tamanhos das pilhas
@@ -52,7 +53,8 @@ void tarefa_8(void);
 #define TAM_PILHA_6			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_7			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_8			(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_9			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_OCIOSA	        (TAM_MINIMO_PILHA + 24)
 
 /*
  * Declaracao das pilhas das tarefas
@@ -65,6 +67,7 @@ uint32_t PILHA_TAREFA_5[TAM_PILHA_5];
 uint32_t PILHA_TAREFA_6[TAM_PILHA_6];
 uint32_t PILHA_TAREFA_7[TAM_PILHA_7];
 uint32_t PILHA_TAREFA_8[TAM_PILHA_8];
+uint32_t PILHA_TAREFA_9[TAM_PILHA_9];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
 
 /*
@@ -245,3 +248,14 @@ void tarefa_8(void)
 		SemaforoLibera(&SemaforoVazio);
 	}
 }
+
+void tarefa_9(void)
+{
+	volatile uint16_t b = 0;
+	for(;;)
+	{
+		b++;
+		TarefaEspera(10);	/* tarefa se coloca em espera por 10 marcas de tempo (ticks) */
+	}
+}
+
